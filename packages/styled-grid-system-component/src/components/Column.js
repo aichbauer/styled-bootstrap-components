fa09432a99d2c0e7045ef4e63bb3881286d21453
Michael Krukov <krukov.michael@ya.ru>
2019-04-25 08:22:58 +0300

    Feat: <Column> now behaves like bootstrap's .col (#34) (closes #33)
    * Feat: <Column> now behaves like bootstrap's .col
    
    * `<Column>` without specified sizes have `flex-grow: 1`;
    * `<Column>` with specified size have `width: 100%`;
    * Values for sizes are now calculated instead of hardcoded;
    * `<Column>` flex-basis now decided from largest specified modificator
    available on current screen size;
    
    * Updated snapshot in `styled-bootstrap-components`

--- packages/styled-grid-system-component/src/components/Column.js
+++ packages/styled-grid-system-component/src/components/Column.js
@@ -1,92 +1,46 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { theme } from 'styled-config';
 
-const calcFlex = (size) => {
-  switch (size) {
-    case 1:
-      return '0 0 8.333333%';
-    case 2:
-      return '0 0 16.666667%';
-    case 3:
-      return '0 0 25%';
-    case 4:
-      return '0 0 33.333333%';
-    case 5:
-      return '0 0 41.666667%';
-    case 6:
-      return '0 0 50%';
-    case 7:
-      return '0 0 58.333333%';
-    case 8:
-      return '0 0 66.666667%';
-    case 9:
-      return '0 0 75%';
-    case 10:
-      return '0 0 83.333333%';
-    case 11:
-      return '0 0 91.666667%';
-    default:
-    case 12:
-      return '0 0 100%';
-  }
-};
+const colums = 12;
 
-export const smFlex = (props) => {
-  if (props.xl || props.md || props.lg) {
-    return '0 0 100%';
-  }
+const percentage = (value) => `${100 * value}%`;
 
-  if (props.sm) {
-    return calcFlex(props.sm);
-  }
-
-  return '0 0 100%';
-};
-
-export const mdFlex = (props) => {
-  if (props.lg || props.xl) {
-    return '0 0 100%';
-  }
-
-  if (props.sm || props.md) {
-    return calcFlex(props.sm || props.md);
-  }
-
-  return '0 0 100%';
-};
-
-export const lgFlex = (props) => {
-  if (props.xl) {
-    return '0 0 100%';
-  }
-
-  if (props.sm || props.md || props.lg) {
-    return calcFlex(props.sm || props.md || props.lg);
-  }
-
-  return '0 0 100%';
-};
+export const columnStyleForSize = (size) => css`
+  flex: 0 0 ${percentage(size / colums)};
+  max-width: ${percentage(size / colums)};
+`;
 
 const Column = styled.div`
   box-sizing: border-box;
   padding-right: 15px;
   padding-left: 15px;
-  flex: 0 0 100%;
   overflow: hidden;
   word-break: break-all;
+
+  ${(props) => (
+    (props.sm || props.md || props.lg || props.xl)
+      ? css`
+          width: 100%;
+        `
+      : css`
+          flex-basis: 0;
+          flex-grow: 1;
+          max-width: 100%;
+      `)}
+
   @media (min-width: ${(props) => props.theme.screenSize.sm}) {
-    flex: ${(props) => smFlex(props)};
-  };
+    ${(props) => props.sm && columnStyleForSize(props.sm)}
+  }
   @media (min-width: ${(props) => props.theme.screenSize.md}) {
-    flex: ${(props) => mdFlex(props)};
-  };
+    ${(props) => props.md && columnStyleForSize(props.md)}
+  }
   @media (min-width: ${(props) => props.theme.screenSize.lg}) {
-    flex: ${(props) => lgFlex(props)};
-  };
+    ${(props) => props.lg && columnStyleForSize(props.lg)}
+  }
   @media (min-width: ${(props) => props.theme.screenSize.xl}) {
-    flex: ${(props) => calcFlex(props.sm || props.md || props.lg || props.xl)};
-  };
+    ${(props) => props.xl && columnStyleForSize(props.xl)}
+  }
 `;
 
 Column.defaultProps = {
---
+++ packages/styled-grid-system-component/test/Column/sizes.spec.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import { Column } from '../../src';
+
+// TODO: how to check @media queries
+test('Styles matches Column with specified sm size', () => {
+  const tree = renderer.create(<Column sm={6} />).toJSON();
+  expect(tree).toMatchSnapshot();
+  expect(tree).toHaveStyleRule('box-sizing', 'border-box');
+  expect(tree).toHaveStyleRule('padding-right', '15px');
+  expect(tree).toHaveStyleRule('padding-left', '15px');
+  expect(tree).toHaveStyleRule('flex-basis', undefined);
+  expect(tree).toHaveStyleRule('flex-grow', undefined);
+  expect(tree).toHaveStyleRule('width', '100%');
+  expect(tree).toHaveStyleRule('overflow', 'hidden');
+  expect(tree).toHaveStyleRule('word-break', 'break-all');
+});
+
+test('Styles matches Column with specified md size', () => {
+  const tree = renderer.create(<Column md={6} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Styles matches Column with specified lg size', () => {
+  const tree = renderer.create(<Column lg={6} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Styles matches Column with specified xl size', () => {
+  const tree = renderer.create(<Column xl={6} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
