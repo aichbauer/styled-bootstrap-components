ebae61a2b4360d04de6f698f57a2cf88262ed4c9
Michael Krukov <krukov.michael@ya.ru>
2019-07-02 20:25:54 +0300

    Updated example for alert (closes #52)

--- .storybook/stories/alerts/alerts/DismissAlert.js
+++ .storybook/stories/alerts/alerts/DismissAlert.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Transition } from '../../../../packages/styled-base-components';
 import { Alert } from '../../../../packages/styled-alert-component';
 import { Container } from '../../../../packages/styled-container-component';
 
@@ -21,23 +22,25 @@ export class DismissAlert extends React.Component {
     const { hidden } = this.state;
     return (
       <Container style={{ textAlign: 'left' }}>
-        <Alert primary dismissible hidden={hidden}>
-          This is a default alert — check it out!
-          <span
-            style={{
-              padding: '15px',
-              position: 'absolute',
-              cursor: 'pointer',
-              right: 0,
-              top: '-3px',
-            }}
-            onClick={() => this.handleDismiss()}
-            onKeyPress={() => this.handleDismiss()}
-            role="button"
-            tabIndex={0}
-          >&times;
-          </span>
-        </Alert>
+        <Transition noInitialEnter hideOnExit hidden={hidden}>
+          <Alert primary dismissible>
+            This is a default alert — check it out!
+            <span
+              style={{
+                padding: '15px',
+                position: 'absolute',
+                cursor: 'pointer',
+                right: 0,
+                top: '-3px',
+              }}
+              onClick={() => this.handleDismiss()}
+              onKeyPress={() => this.handleDismiss()}
+              role="button"
+              tabIndex={0}
+            >&times;
+            </span>
+          </Alert>
+        </Transition>
       </Container>
     );
   }
