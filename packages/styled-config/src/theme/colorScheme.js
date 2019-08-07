19fa4b7054af2eb8633bc6212060432d1ad2ee7c
Michael Krukov <krukov.michael@ya.ru>
2019-08-07 18:38:35 +0300

    Updated colorScheme

--- packages/styled-config/src/theme/colorScheme.js
+++ packages/styled-config/src/theme/colorScheme.js
@@ -1,16 +1,62 @@
-import { colors } from './colors';
-
 const colorScheme = {
-  primary: colors.blue,
-  secondary: colors.gray,
-  success: colors.green,
-  danger: colors.red,
-  warning: colors.yellow,
-  info: colors.cyan,
-  light: colors.gray100,
-  dark: colors.gray800,
-  darker: colors.gray900,
-  white: colors.white,
+  primaryLight: '#cce5ff',
+  primaryLighter: '#9cc9fb',
+  primary: '#007bff',
+  primaryDarker: '#0069d9',
+  primaryDark: '#005cbf',
+
+  secondaryLight: '#e2e3e5',
+  secondaryLighter: '#b4b5b7',
+  secondary: '#6c757d',
+  secondaryDarker: '#5a6268',
+  secondaryDark: '#4e555b',
+
+  successLight: '#d4edda',
+  successLighter: '#72d889',
+  success: '#28a745',
+  successDarker: '#218838',
+  successDark: '#1c7430',
+
+  dangerLight: '#f8d7da',
+  dangerLighter: '#f5b4bb',
+  danger: '#dc3545',
+  dangerDarker: '#c82333',
+  dangerDark: '#b21f2d',
+
+  warningLight: '#fff3cd',
+  warningLighter: '#f3da8d',
+  warning: '#ffc107',
+  warningDarker: '#e0a800',
+  warningDark: '#c69500',
+
+  infoLight: '#d1ecf1',
+  infoLighter: '#a3d6de',
+  info: '#17a2b8',
+  infoDarker: '#138496',
+  infoDark: '#10707f',
+
+  lightLight: '#fefefe',
+  lightLighter: '#fdfdfe',
+  light: '#f0f0f0',
+  lightDarker: '#e2e6ea',
+  lightDark: '#d3d9df',
+
+  darkLight: '#d6d8d9',
+  darkLighter: '#c6c8ca',
+  dark: '#343a40',
+  darkDarker: '#23272b',
+  darkDark: '#171a1d',
+
+  darkerLight: '#4d4f50',
+  darkerLighter: '#3c3e40',
+  darker: '#2d2f31',
+  darkerDarker: '#212529',
+  darkerDark: '#16181b',
+
+  white: '#fff',
+  whiteDarker: '#f4f4f4',
+  whiteDark: '#ececec',
+
   transparent: 'transparent',
 };
 
