97c72de48a521041261527241a0c1b21c35903f7
Bryan Paronto <bparonto@gmail.com>
2020-02-08 23:19:39 -0600

    addspadding to container and negative margin to rows to align with BS4 grid

--- packages/styled-container-component/src/components/Container.js
+++ packages/styled-container-component/src/components/Container.js
@@ -8,79 +8,78 @@ import {
   getFontFamily,
   getFontSize,
   getFontWeight,
-  getBreakpointSize,
+  getBreakpointSize
 } from 'styled-config';
 
 const Container = styled(Div)`
-  font-family: ${(props) => getFontFamily(props, 'container', 'default')};
-  width: ${(props) => getWidth(props, 'container', 'default')};
+  font-family: ${props => getFontFamily(props, 'container', 'default')};
+  width: ${props => getWidth(props, 'container', 'default')};
   margin-right: auto;
   margin-left: auto;
-  font-size:  ${(props) => getFontSize(props, 'container', 'default')};
-  font-weight: ${(props) => getFontWeight(props, 'container', 'default')};
+  font-size: ${props => getFontSize(props, 'container', 'default')};
+  font-weight: ${props => getFontWeight(props, 'container', 'default')};
   line-height: 1.5;
   box-sizing: border-box;
-  @media (min-width: ${(props) => {
-    const screenWidth = getBreakpointSize({
-      ...props,
-      sm: true,
-    });
+  padding-left: 15px;
+  padding-right: 15px;
 
-    return screenWidth;
-  }}) {
-    max-width: ${(props) => (
-    props.fluid
-      ? getWidth(props, 'container', 'default')
-      : getWidth(props, 'container', 'fluid', 'sm')
-  )};
+  @media (min-width: ${props => {
+      const screenWidth = getBreakpointSize({
+        ...props,
+        sm: true
+      });
+
+      return screenWidth;
+    }}) {
+    max-width: ${props =>
+      props.fluid
+        ? getWidth(props, 'container', 'default')
+        : getWidth(props, 'container', 'fluid', 'sm')};
     box-sizing: border-box;
-  };
-  @media (min-width: ${(props) => {
-    const screenWidth = getBreakpointSize({
-      ...props,
-      md: true,
-    });
+  }
+  @media (min-width: ${props => {
+      const screenWidth = getBreakpointSize({
+        ...props,
+        md: true
+      });
 
-    return screenWidth;
-  }}) {
-    max-width: ${(props) => (
-    props.fluid
-      ? getWidth(props, 'container', 'default')
-      : getWidth(props, 'container', 'fluid', 'md')
-  )};
-  };
-  @media (min-width: ${(props) => {
-    const screenWidth = getBreakpointSize({
-      ...props,
-      lg: true,
-    });
+      return screenWidth;
+    }}) {
+    max-width: ${props =>
+      props.fluid
+        ? getWidth(props, 'container', 'default')
+        : getWidth(props, 'container', 'fluid', 'md')};
+  }
+  @media (min-width: ${props => {
+      const screenWidth = getBreakpointSize({
+        ...props,
+        lg: true
+      });
 
-    return screenWidth;
-  }}) {
-    max-width: ${(props) => (
-    props.fluid
-      ? getWidth(props, 'container', 'default')
-      : getWidth(props, 'container', 'fluid', 'lg')
-  )};
-  };
-  @media (min-width: ${(props) => {
-    const screenWidth = getBreakpointSize({
-      ...props,
-      xl: true,
-    });
+      return screenWidth;
+    }}) {
+    max-width: ${props =>
+      props.fluid
+        ? getWidth(props, 'container', 'default')
+        : getWidth(props, 'container', 'fluid', 'lg')};
+  }
+  @media (min-width: ${props => {
+      const screenWidth = getBreakpointSize({
+        ...props,
+        xl: true
+      });
 
-    return screenWidth;
-  }}) {
-    max-width: ${(props) => (
-    props.fluid
-      ? getWidth(props, 'container', 'default')
-      : getWidth(props, 'container', 'fluid', 'xl')
-  )};
-  };
+      return screenWidth;
+    }}) {
+    max-width: ${props =>
+      props.fluid
+        ? getWidth(props, 'container', 'default')
+        : getWidth(props, 'container', 'fluid', 'xl')};
+  }
 `;
 
 Container.defaultProps = {
-  theme,
+  theme
 };
 
 export { Container };
